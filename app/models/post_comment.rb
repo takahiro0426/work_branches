c9ea897dd15a8b7de1962cba48cf509725372817
vagrant <vagrant@localhost.localdomain>
2020-11-22 23:31:43 +0000

    close #26

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -2,5 +2,5 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :community_post
 
-  validates :comment, presence: true, length: { maximum: 18 }
+  validates :comment, presence: true, length: { maximum: 30 }
 end
