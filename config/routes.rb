5d9ce01364fb7cec859d9eb31d765a8adde6ffe4
vagrant <vagrant@localhost.localdomain>
2021-02-05 15:30:25 +0000

    Clean post_comments

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 	resources :user_communities, only: [:new, :create, :update, :index]
 	resources :communities
 	resources :community_posts
-  resources :post_comment, only: [:edet, :update, :show, :delete]
+  resources :post_comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
